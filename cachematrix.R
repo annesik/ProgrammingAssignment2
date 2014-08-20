a18f1f640c65b7405bb9d378920a3c76d37ca53e
annesik <annesik@Anne-Siri-Koksruds-MacBook-Pro.local>
2014-08-20 21:13:15 +0200

    Wrote makeCacheMatrix and cacheSolve functions
    Wrote the makeCacheMatrix and cacheSolve functions as specified in the
    R programming assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## My overall description of what my
-## functions do
+## This file contains functions to calculate the inverse of a matrix, or alternatively 
+## retrieve the inverse of the matrix from the cache if it has already been calculated.
 
-## Write a short comment describing this function
+## The makeCacheMatrix function creates a special "matrix" object that can 
+## cache the inverse of the matrix given as an argument. The object returend is
+## really a list containing four functions to
+##    1. set the value of the matrix
+##    2. get the value of the matrix
+##    3. set the value of the inverse
+##    4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      m <- NULL
+      set <- function(y) {
+            x <<- y
+            m <<- NULL
+      }
+      get <- function() x
+      setinv <- function(inv) m <<- inv
+      getinv <- function() m
+      list(set = set, get = get,
+           setinv = setinv,
+           getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## The following function calculates the inverse of the special "matrix" 
+## created with the above function. However, it first checks to see if the inverse 
+## has already been calculated. If so, it gets the inverse from the cache and skips 
+## the computation. Otherwise, it calculates the inverse of the data and sets the 
+## value of the inverse in the cache via the setinv function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      m <- x$getinv()
+      if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setinv(m)
+      m
 }
